b5788e84cb81f0e983097a905f938199cc575448
Hanseul Shim <hanseulshim@gmail.com>
2020-11-19 16:26:55 -0500

    Add authenticated Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react'
+import { useAuth } from 'components/auth/AuthProvider'
 
 const Header: React.FC = () => {
+  const {
+    auth: { user }
+  } = useAuth()
+
   return (
     <div id="header">
-      <div className="header-spacer">
+      <div className="header-spacer header-row">
         <img src={`${process.env.baseUrl}/header-logo.svg`} alt="logo" />
+        <div className="profile-row">
+          <span>
+            Hello {user.name_first} {user.name_last}
+          </span>
+          <img id="profile" alt="profile" src="/profile.svg" />
+        </div>
       </div>
       <div className="header-spacer primary-background">
         <h2>Radian Interactive Value</h2>
--- components/auth/AuthProvider.tsx
+++ components/auth/AuthProvider.tsx
@@ -1,6 +1,54 @@
 import React, { useState, useEffect, createContext, useContext } from 'react'
 import Cookies from 'js-cookie'
 
+const testAuth = {
+  user: {
+    userid_ssid: '16252-5',
+    roleid: 0,
+    clientcode: 'string',
+    departmentid: 0,
+    username: 'string',
+    email: 'string',
+    name_last: 'McCringleberry',
+    name_first: 'Hingle',
+    address: 'string',
+    city: 'string',
+    state: 'string',
+    zip: 'string',
+    phone_home: '215-555-1872',
+    phone_mobile: '484-555-0980',
+    title: 'string'
+  },
+  token: 'bcf62b2c-c739-42aa-958d-7a9930ca7fff',
+  redirect: 'string',
+  status: 0,
+  message: 'string'
+}
+
+const defaultAuth = {
+  user: {
+    userid_ssid: '',
+    roleid: 0,
+    clientcode: '',
+    departmentid: 0,
+    username: '',
+    email: '',
+    name_last: '',
+    name_first: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+    phone_home: '',
+    phone_mobile: '',
+    title: ''
+  },
+  token: '',
+  redirect: '',
+  status: 0,
+  message: ''
+}
+
 type AuthType = {
   user: {
     userid_ssid: string
@@ -42,11 +90,12 @@ export const AuthProvider = ({
 }: {
   children: React.ReactNode
 }): React.ReactElement => {
-  const [auth, setAuth] = useState<AuthType>(null)
+  const [auth, setAuth] = useState<AuthType>(defaultAuth)
   const [isLoading, setLoading] = React.useState<boolean>(true)
 
   useEffect(() => {
     const initializeAuth = async (): Promise<void> => {
+      Cookies.set('auth', testAuth)
       const authCookies = Cookies.get('auth')
       if (authCookies) {
         setAuth(JSON.parse(authCookies))
