18a4ec718aeacb6fbe76a45f65db4ffdb400a1e4
joneil@boostlabs.com <joneil@boostlabs.com>
2020-11-24 15:48:45 -0600

    add pathing

--- components/login/TermsOfUse.tsx
+++ components/login/TermsOfUse.tsx
@@ -2,7 +2,7 @@ const TermsOfUse: React.FC = () => (
   <>
     <h4>Terms of Use:</h4>
     <object
-      data="/TermsOfUse.pdf"
+      data={`${process.env.baseUrl}/TermsOfUse.pdf`}
       type="application/pdf"
       width="100%"
       height="300px"
