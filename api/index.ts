7f98afe5b9f170b5e5641aeabca4f840a93532a0
Hanseul Shim <hanseulshim@gmail.com>
2020-12-11 10:26:10 -0500

    Return data or message

--- api/index.ts
+++ api/index.ts
@@ -30,5 +30,5 @@ export const handleApi = async (route: string, payload?: any) => {
   } else if (status !== 1) {
     throw new Error(message)
   }
-  return data
+  return data ? data : message
 }
