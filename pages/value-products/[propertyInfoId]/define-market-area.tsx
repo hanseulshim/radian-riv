abb124f3302873ae5af8e9454efdb586e8001367
John O'Neil <joneil@boostlabs.com>
2021-02-11 08:13:29 -0600

    Broken routes (#103)
    * Riv 064 - Change Property Characteristics
    
    * merge export pdf in
    
    * changes that are ruining my life
    
    * add date validation for input
    
    * add default form values and api calls
    
    * starting table for change prop chars
    
    * add prop chars table
    
    * breakout change prop components
    
    * add validation for year built
    
    * update validation per Kevin
    
    * build payload for update call
    
    * correctly building payload with source call
    
    * add additional property info
    
    * add api call to post new property info
    
    * update options to strings in local state
    
    * Rename for the boys
    
    * add unit test
    
    * update broken unit test
    
    * add e2e tests
    
    * remove unused onClick
    
    * update tests and snapshots
    
    * filename casing and type error conflict
    
    * fix sidebar filename casing issues
    
    * casing
    
    * casing
    
    * Fix broken routes in value products

---
+++ pages/value-products/[propertyInfoId]/define-market-area.tsx
@@ -0,0 +1,12 @@
+import ValueProductLayout from 'components/layouts/ValueProductLayout'
+import React from 'react'
+
+interface Props {}
+
+export default function DefineMarketArea({}: Props) {
+  return (
+    <ValueProductLayout>
+      <h1>Define Market Area</h1>
+    </ValueProductLayout>
+  )
+}
---
+++ pages/value-products/[propertyInfoId]/valuation-form.tsx
@@ -0,0 +1,12 @@
+import ValueProductLayout from 'components/layouts/ValueProductLayout'
+import React from 'react'
+
+interface Props {}
+
+export default function ValuationForm({}: Props) {
+  return (
+    <ValueProductLayout>
+      <h1>RIV Valuation Form</h1>
+    </ValueProductLayout>
+  )
+}
